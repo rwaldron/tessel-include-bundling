86087ba0436c5a6d1e9d00043eae24d2a3be3de6
Rick Waldron <waldron.rick@gmail.com>
2015-11-13 12:44:54 -0500

    whatever

---
+++ index.js
@@ -0,0 +1,36 @@
+var fs = require("fs");
+var path = require("path");
+var http = require("http");
+var os = require("os");
+var networkInterfaces = os.networkInterfaces();
+
+var port = 8080;
+var host = networkInterfaces.wlan0[0].address;
+var publicpath = path.join(__dirname, "public");
+var header = { "Content-Type": null };
+var contentType = "Content-Type";
+var mimeTypes = {
+  ".html": "text/html",
+  ".jpeg": "image/jpeg",
+  ".jpg": "image/jpeg",
+  ".png": "image/png",
+  ".js": "text/javascript",
+  ".css": "text/css",
+};
+
+var server = http.createServer(function(request, response) {
+  var file = path.join(publicpath, request.url === "/" ? "index.html" : request.url);
+  var fstream = fs.createReadStream(file);
+
+  fstream.on("open", function() {
+    header[contentType] = mimeTypes[path.extname(this.path)];
+    response.writeHead(200, header);
+    fstream.pipe(response);
+  });
+
+  console.log(fs.readFileSync(path.join(__dirname, "node_modules/foo/package.json")));
+});
+
+server.listen(port, host);
+
+console.log("%s:%s", host, port);
